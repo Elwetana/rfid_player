4d014dbfedf6b05dcc8ee5c84aa8b77f911166dd
Elwetana <martin@argenite.org>
2015-02-01 13:10:40 +0000

    More statistics in database

--- src/create_db.py
+++ src/create_db.py
@@ -3,8 +3,10 @@
 import sqlite3
 
 con = sqlite3.connect('player.db')
-con.execute("drop table if exists lastpos")
-con.execute("create table lastpos (foldername varchar primary key, fileindex integer, position integer)")
+#con.execute("drop table if exists lastpos")
+#con.execute("create table lastpos (foldername varchar primary key, fileindex integer, position integer)")
+#con.execute('alter table lastpos add column completed integer')
+con.execute('update lastpos set completed=0')
 con.commit()
 
 for row in con.execute("select * from lastpos"):
